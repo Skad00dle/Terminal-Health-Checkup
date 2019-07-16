11eee70bb20821e4ba8ceacaa7cbb4079084f8b8
Rajkaran <karanramesh96@gmail.com>
2019-07-16 10:56:36 +0530

    added extra apis for fetching data

--- controllers/TerminalHealthController.go
+++ controllers/TerminalHealthController.go
@@ -1,3 +1,22 @@
 package controllers
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"miniProject/models"
+	DbUtility "miniProject/pkg"
+	"net/http"
+)
 
+func FetchTerminalHealth(ctx *gin.Context)  {
+	fmt.Println("fetching all terminals")
+	var terminalHealths []models.TerminalHealth
+	DbUtility.DB.Where("terminal_id = ? ",ctx.Param("id") ).Find(&terminalHealths)
+	if len(terminalHealths) <= 0 {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No terminals found!"})
+		return
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": terminalHealths})
+	}
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -13,6 +13,14 @@ func InitiateRoutes()  {
 	{
 		v1.POST("/", Controller.AddTerminal)
 		v1.GET("/", Controller.FetchTerminals)
+		v1.GET("/:id/getHealth", Controller.FetchTerminalHealth)
+		//v1.GET("/:id", fetchSingleTodo)
+		//v1.PUT("/:id", updateTodo)
+		//v1.DELETE("/:id", deleteTodo)
+	}
+	v2 := router.Group("/terminalHealth")
+	{
+		v2.GET("/:id/details", Controller.FetchTerminalHealthHit)
 		//v1.GET("/:id", fetchSingleTodo)
 		//v1.PUT("/:id", updateTodo)
 		//v1.DELETE("/:id", deleteTodo)
